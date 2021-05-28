4489be942dbc4f582c38c67f4f6a629745c03abf
Soledad Li Wang <soledad.liwang@globant.com>
2021-05-28 15:18:35 -0300

    Section 12: Routing

---
+++ CookBook/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+
+const appRoutes: Routes = [
+  { path:'', redirectTo: '/recipe', pathMatch: 'full' },
+  { path:'recipe', component: RecipeComponent, children: [
+    {path:'', component: RecipeStartComponent},
+    {path:'new', component: RecipeEditComponent},
+    {path:':id', component: RecipeDetailComponent},
+    {path:':id/edit', component: RecipeEditComponent},
+  ]},
+  { path:'shopping-list', component: ShoppingListComponent},
+  
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+
+}
--- CookBook/src/app/recipe/recipe.service.ts
+++ CookBook/src/app/recipe/recipe.service.ts
@@ -39,8 +39,12 @@ export class RecipeService{
 
 constructor(private slService: ShoppingListService){}
 
-getRecipe(){
+getRecipes(){
     return this.recipes.slice();
+}
+
+getRecipe(index: number) {
+    return this.recipes[index]
 }
 
 addIngredientsToSl(ingredients: Ingredient[]){
