64a2cf4f1d5247a545ad6cdf09da5e67f37f557c
Soledad Li Wang <soledad.liwang@globant.com>
2021-05-28 17:21:47 -0300

    Section 14: Observables

--- CookBook/src/app/app.component.ts
+++ CookBook/src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe'
-  title = 'CookBook';
-
-  onNavigate(feature: string){
-    this.loadedFeature = feature;
-  }
 }
--- CookBook/src/app/shopping-list/shopping-list.component.ts
+++ CookBook/src/app/shopping-list/shopping-list.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
 
-ingredients : Ingredient[];
+export class ShoppingListComponent implements OnInit, OnDestroy {
 
-constructor(private slService: ShoppingListService) { }
+  ingredients : Ingredient[];
+  private subscription: Subscription;
 
-ngOnInit() {
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
   this.ingredients = this.slService.getIngredients();
-  this.slService.ingredientChanged.subscribe(
+  this.subscription = this.slService.ingredientChanged.subscribe(
     (ingredients : Ingredient[]) => {
       this.ingredients = ingredients;
     }
   )
-}
+  }
 
+  ngOnDestroy(){
+  this.subscription.unsubscribe();
+  }
 }
