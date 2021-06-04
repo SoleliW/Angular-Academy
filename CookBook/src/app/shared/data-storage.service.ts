b1584a5df39c86608a0c3018b5ec255d668e98d0
Soledad Li Wang <soledad.liwang@globant.com>
2021-06-04 10:46:51 -0300

    Attaching token with interceptor and added logout

--- CookBook/src/app/shared/data-storage.service.ts
+++ CookBook/src/app/shared/data-storage.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map, tap } from "rxjs/operators";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
 
 import { Recipe } from "../recipe/recipe.model";
 import { RecipeService } from "../recipe/recipe.service";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { AuthService } from "../auth/auth.service";
 
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService, private shoppingListService: ShoppingListService) {}
+    constructor(private http: HttpClient, 
+                private recipeService: RecipeService, 
+                private authService: AuthService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -22,14 +24,15 @@ export class DataStorageService {
 
     fetchRecipes() {
         return this.http.get<Recipe[]>('https://angular-academy-c2630-default-rtdb.firebaseio.com/recipes.json')
-            .pipe(map(recipes => {
+        .pipe(map(recipes => {
                 return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
-                }; 
+                    return {...recipe, 
+                        ingredients: recipe.ingredients ? recipe.ingredients : []
+                };
             });
         }),
         tap(recipes => {
             this.recipeService.setRecipes(recipes);
-        }))
+        }));
     }
 }
