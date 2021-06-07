b227cef2dd55852657b1b707cdf3d837b1bd0b02
Soledad Li Wang <soledad.liwang@globant.com>
2021-06-07 14:36:02 -0300

    Core Module added

--- CookBook/src/app/app.module.ts
+++ CookBook/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipe/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
 import { RecipeModule } from './recipe/recipe.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -29,15 +26,8 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     RecipeModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [ShoppingListService, 
-    RecipeService, 
-    {
-      provide: HTTP_INTERCEPTORS, 
-      useClass: AuthInterceptorService,
-      multi:true
-    }
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent],
 })
