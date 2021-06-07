47df296463fc93a6e4dd62178eba6fa7303c2b15
Soledad Li Wang <soledad.liwang@globant.com>
2021-06-07 12:21:10 -0300

    Dynamic Components

--- CookBook/src/app/auth/auth.component.ts
+++ CookBook/src/app/auth/auth.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { AuthService, AuthResponseData } from './auth.service';
-
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
 
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  private closeSub: Subscription;
+  constructor(private authService: AuthService, private router: Router, 
+              private componentFactoryResolver: ComponentFactoryResolver) {}
 
   authForm = new FormGroup({
     "email": new FormControl("", [Validators.required, Validators.email]),
@@ -51,11 +55,36 @@ export class AuthComponent {
     errorMessage => {
       console.log(errorMessage);
       this.error = errorMessage;
+      this.showErrorAlert(errorMessage);
       this.isLoading = false;
     }
     );
 
     this.authForm.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub){
+      this.closeSub.unsubscribe();
+    }
+  }
+
+  private showErrorAlert(message: string) {
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe( () => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    })
   }
 
 }
