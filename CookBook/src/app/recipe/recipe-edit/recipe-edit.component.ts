99e965bad9ab4061ae72d2fcae4dd54b507422b4
Soledad Li Wang <soledad.liwang@globant.com>
2021-06-09 11:38:41 -0300

    fix: recipe edit delete ingredient

--- CookBook/src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ CookBook/src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -58,7 +58,7 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onDeleteIngredient(index: number) {
-    (<FormArray>this.recipeForm.get('ingredients')).clear();
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
   }
 
   onCancel() {
